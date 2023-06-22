e7f1bcee314a4b49ab00737e324f84d4cbf552a5
Carmelia Shito <carmshito@hotmail.com>
2023-06-22 16:55:10 -0400

    implement AJAX to send tweet text to server and fetch data from server, included timeago library to show time passed instead of numbers

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,31 +6,7 @@
 
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+  const data = [];
 
   const renderTweets = function(tweets) {
     // loops through tweets
@@ -38,7 +14,7 @@ $(document).ready(function() {
       // calls createTweetElement for each tweet
       const $tweet = createTweetElement(tweet);
       // takes return value and appends it to the tweets container
-      $('#all-tweets').append($tweet);
+      $('#all-tweets').prepend($tweet);
     }
   };
 
@@ -55,33 +31,59 @@ $(document).ready(function() {
         <p>${tweetData.content.text}</p>
       </div>
       <footer>
-        <span class="time-stamp">${tweetData.created_at}</span>
+        <span class="time-stamp">${timeago.format(tweetData.created_at)}</span>
         <div class="icons">
-          <i class="fa-sharp fa-solid fa-flag"></i>
-          <i class="fa-solid fa-retweet"></i>
-          <i class="fa-solid fa-heart"></i>
+        <i class="fa-sharp fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
         </div>
-      </footer>
-    </article>`
+        </footer>
+        </article>`
     );
     return $tweet;
   };
 
   renderTweets(data);
 
+  // function responsible for fetching tweets from /tweets
+  const loadTweets = function() {
+    const url = `http://localhost:8080/tweets`;
+    $.ajax({
+      url,
+      method: "GET",
+      dataType: "json",
+      success: (tweets) => {
+        renderTweets(tweets);
+      }
+    });
+  };
+
+  loadTweets();
+
   // submit tweet event
   // add an event listener that listens for the submit event
-  $('#tweet-form').on('submit', (event) => {
+  $('#tweet-form').on('submit', function(event) {
 
     // prevent the default behaviour of the submit event (data submission and page refresh)
     event.preventDefault();
-    
+
+    const $formData = $('#tweet-form');
+    console.log($formData);
+
+    // Serialize the form data
+    const data = $formData.serialize();
+    console.log(data);
+
     // create an AJAX POST request in client.js that sends the form data to the server.
-    $.ajax("/tweets", {
-      method: "POST",
-      // Serialize the form data
-      data: $("#tweet-text").serialize(),
-    });
-    $('#tweet-text').val("");
+    $.post("/tweets", data)
+      .then(res => {
+        loadTweets();
+        $('#tweet-text').val('');
+      })
+      .catch(err => {
+        console.log(err);
+      });
   });
+
+
 });
